e9b051a55d1299d7e753f0cbbe4633e2fa4f3a2e
Jo Colina <jo.colina@weezevent.com>
2022-03-27 16:16:58 +0200

    Feature: add automatic help

--- test/argumentate.js
+++ test/argumentate.js
@@ -1,3 +1,4 @@
+const Sinon = require('sinon');
 const { expect } = require('chai');
 
 const Argumentate = require('../argumentate');
@@ -5,7 +6,7 @@ const Argumentate = require('../argumentate');
 describe('Argumentate', () => {
 	it('Should be able to understand a simple option', () => {
 		let args = ['-c'];
-		let argumentate = Argumentate(args);
+		let argumentate = Argumentate({ args });
 
 		expect(argumentate).to.have.property('options');
 		expect(Object.keys(argumentate.options).length).to.be.eql(1);
@@ -15,7 +16,7 @@ describe('Argumentate', () => {
 
 	it('Should be able to understand a simple option, mapped', () => {
 		let args = ['-c'];
-		let argumentate = Argumentate(args, { c: 'config' });
+		let argumentate = Argumentate({ args, mapping: { c: 'config' } });
 
 		expect(argumentate).to.have.property('options');
 		expect(Object.keys(argumentate.options).length).to.be.eql(1);
@@ -25,7 +26,7 @@ describe('Argumentate', () => {
 
 	it('Should be able to understand a simple variable', () => {
 		let args = ['start'];
-		let argumentate = Argumentate(args);
+		let argumentate = Argumentate({ args });
 
 		expect(argumentate).to.have.property('variables');
 		expect(argumentate.variables).to.have.length(1);
@@ -34,7 +35,7 @@ describe('Argumentate', () => {
 
 	it('Should be able to understand a simple option and a simple variable', () => {
 		let args = ['start', '-c'];
-		let argumentate = Argumentate(args);
+		let argumentate = Argumentate({ args });
 
 		expect(argumentate).to.have.property('options');
 		expect(Object.keys(argumentate.options).length).to.be.eql(1);
@@ -48,7 +49,7 @@ describe('Argumentate', () => {
 
 	it('Should be able to understand a simple option with value (spaced)', () => {
 		let args = ['-c', 'start'];
-		let argumentate = Argumentate(args);
+		let argumentate = Argumentate({ args });
 
 		expect(argumentate).to.have.property('options');
 		expect(Object.keys(argumentate.options).length).to.be.eql(1);
@@ -58,7 +59,7 @@ describe('Argumentate', () => {
 
 	it('Should be able to understand a simple option with value (=)', () => {
 		let args = ['-c=start'];
-		let argumentate = Argumentate(args);
+		let argumentate = Argumentate({ args });
 
 		expect(argumentate).to.have.property('options');
 		expect(Object.keys(argumentate.options).length).to.be.eql(1);
@@ -68,7 +69,7 @@ describe('Argumentate', () => {
 
 	it('Should be able to understand many identical options with value (=)/ without value', () => {
 		let args = ['-c=start', '-c', 'plep', '-c=45'];
-		let argumentate = Argumentate(args);
+		let argumentate = Argumentate({ args });
 
 		expect(argumentate).to.have.property('options');
 		expect(Object.keys(argumentate.options).length).to.be.eql(1);
@@ -79,7 +80,7 @@ describe('Argumentate', () => {
 
 	it('Should be able to understand a simple option with value (spaced)', () => {
 		let args = ['start', '-c', 'poulet'];
-		let argumentate = Argumentate(args);
+		let argumentate = Argumentate({ args });
 
 		expect(argumentate).to.have.property('options');
 		expect(Object.keys(argumentate.options).length).to.be.eql(1);
@@ -93,7 +94,7 @@ describe('Argumentate', () => {
 
 	it('Should be able to understand a simple option with value (spaced & mapped)', () => {
 		let args = ['start', '-c', 'poulet'];
-		let argumentate = Argumentate(args, { c: 'config' });
+		let argumentate = Argumentate({ args, mapping: { c: 'config' }});
 
 		expect(argumentate).to.have.property('options');
 		expect(Object.keys(argumentate.options).length).to.be.eql(1);
@@ -103,5 +104,31 @@ describe('Argumentate', () => {
 		expect(argumentate).to.have.property('variables');
 		expect(argumentate.variables).to.have.length(1);
 		expect(argumentate.variables[0]).to.be.eql('start');
+	});
+
+	for(const arg of ['-h', '--help']) {
+		it(`Should automatically call the help function if ${arg} is used`, () => {
+			let args = [arg];
+			let help = Sinon.fake();
+			Argumentate({ args, config: { help, autoExit: false }});
+			expect(help.calledOnce).to.be.true;
+		});
+	}
+
+	for(const arg of ['-o', '--ooo']) {
+		it(`Should call the help function with custom arg ${arg}`, () => {
+			let args = [arg];
+			let help = Sinon.fake();
+			Argumentate({args, config: { help, autoExit: false, helpKey: 'o', helpWord: 'ooo' }});
+			expect(help.calledOnce).to.be.true;
+		});
+	}
+
+	it('Should not call the help function if useHelp is false', () => {
+		const args = ['-h'];
+		const help = Sinon.fake();
+		Argumentate({args, config: { help, autoExit: false, useHelp: false  }});
+
+		expect(help.callCount).to.be.eql(0);
 	});
 });
